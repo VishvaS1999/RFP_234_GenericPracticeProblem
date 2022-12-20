7d7800ac33f23ce2fd5d62771ec75769d2c151e0
VishvaS1999 <gvishva2601@gmail.com>
2022-12-20 23:53:20 +0530

    Find Float Value

---
+++ src/com/generic/UC2_FindFloatValue.java
@@ -0,0 +1,33 @@
+package com.generic;
+
+public class UC2_FindFloatValue
+{
+    public static Integer testMaximum(Integer x, Integer y, Integer z)
+    {
+        Integer max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+
+    public static Float testMaximum(Float x, Float y, Float z)
+    {
+        Float max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to test maximum of three numbers");
+        System.out.println("The maximum of integer is: " +testMaximum(30,20,15));
+        System.out.println("The maximum of float is: " +testMaximum(4.9f, 1.7f, 9.6f));
+    }
+}
