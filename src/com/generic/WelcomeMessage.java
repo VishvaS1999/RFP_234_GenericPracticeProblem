91be73d81c3c9e04d6a458906edda7b15a565033
VishvaS1999 <gvishva2601@gmail.com>
2022-12-20 23:50:35 +0530

    Welcome Generic Program

---
+++ src/com/generic/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package com.generic;
+
+public class WelcomeMessage
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome Generic Practice Program!..");
+    }
+}
